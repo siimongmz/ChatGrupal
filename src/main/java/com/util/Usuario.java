5129cb748c96e6ad77c424959c09c50dd3335cb6
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-03-22 13:32:57 +0100

    El Usuario ahora almacena una imagen en base64

--- src/main/java/com/util/Usuario.java
+++ src/main/java/com/util/Usuario.java
@@ -2,14 +2,23 @@ package com.util;
 
 import lombok.Getter;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.Base64;
 
 public class Usuario implements Serializable {
     @Getter
     private final String nombreUsuario;
+    @Getter
+    private String base64Perfil;
 
-    public Usuario(String nombreUsuario) {
+    public Usuario(String nombreUsuario, String base64Perfil) {
         this.nombreUsuario = nombreUsuario;
+        this.base64Perfil = base64Perfil;
     }
+
+
+
+    //https://github.com/loizenai/image-to-base-64-java-8
+
 
 }
